182247854cefa53681416de396d1c69c6ba21e40
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-03-01 20:55:13 +0100

    init

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from numlib import interpolation as i
+from numlib import interpolation as interpolate
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 #x=np.array([1,2,3,4,5])
-#y=np.array([1,2,3,3,5])
+y=np.array([1,2,3,3,5])
+z=np.prod(y)
+print(z)
 #plt.plot(x,y)
 #plt.show()
-i.test()
