89729dc2f8ab4e961473d75551c73533c273b5fb
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-03-01 17:40:00 +0100

    init

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from numlib import interpolation as i
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-x=np.array([1,2,3,4,5])
-y=np.array([1,2,3,3,5])
-plt.plot(x,y)
-plt.ylabel('some numbers')
-plt.show()
+#x=np.array([1,2,3,4,5])
+#y=np.array([1,2,3,3,5])
+#plt.plot(x,y)
+#plt.show()
+i.test()
