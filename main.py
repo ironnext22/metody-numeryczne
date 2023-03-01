523cc1fb01e05974ffc01619a0056a59794f1650
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-03-01 15:24:20 +0100

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+x=np.array([1,2,3,4,5])
+y=np.array([1,2,3,3,5])
+plt.plot(x,y)
+plt.ylabel('some numbers')
+plt.show()
