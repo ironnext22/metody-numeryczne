224cfcac467dfe6f4b77598a083fdbaac2e92981
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-03-03 21:28:22 +0100

    interpolacja 1 działa

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from numlib import interpolation as interpolate
+from numlib import interpolation as inter
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-#x=np.array([1,2,3,4,5])
-y=np.array([1,2,3,3,5])
-z=np.prod(y)
-print(z)
-#plt.plot(x,y)
-#plt.show()
+x=np.array([-5,-4,-3,-2,-1,0,1,2,3,4,5])
+y=np.array([25,16,9,4,25,0,1,4,9,2,25])
+z=np.linspace(-5,5,100)
+fig, ax = plt.subplots()
+ax.plot(z, inter.Lagrange(x, y, z), label="Funkcja interpolacyjna")
+ax.scatter(x, y, label="Węzły interpolacji")
+ax.legend()
+plt.show()
--- numlib/interpolation.py
+++ numlib/interpolation.py
@@ -1,1 +1,12 @@
-#ef Lagrange():
+def Lagrange(x,y,z):
+    n=len(x)
+    if n!= len(y):
+        raise ValueError("Lista x i y muszą mieć taką samą długość")
+    s=0
+    for i in range(n):
+        p=1
+        for j in range(n):
+            if j != i:
+                p *= (z-x[j])/(x[i]-x[j])
+        s += y[i]*p
+    return s
