040abb5c168cfe638c799fd10c59c3765293658b
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-03-03 21:35:20 +0100

    interpolacja 1 i 2 działa

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from numlib import interpolation as inter
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-x=np.array([-5,-4,-3,-2,-1,0,1,2,3,4,5])
-y=np.array([25,16,9,4,25,0,1,4,9,2,25])
-z=np.linspace(-5,5,100)
+
+
+# x=np.array([-5,-4,-3,-2,-1,0,1,2,3,4,5])
+# y=np.array([25,16,9,4,25,0,1,4,9,2,25])
+# z=np.linspace(-5,5,100)
+# fig, ax = plt.subplots()
+# ax.plot(z, inter.Lagrange(x, y, z), label="Funkcja interpolacyjna")
+# ax.scatter(x, y, label="Węzły interpolacji")
+# ax.legend()
+# plt.show()
+
+x = [0, 1, 2, 3, 4, 5]
+y = [0, 1, 4, 9, 16, 25]
+
+# Punkty do wygenerowania wykresu funkcji interpolacyjnej
+z = np.linspace(0, 5, 100)
+
+# Wykonanie interpolacji i generacja wykresu
 fig, ax = plt.subplots()
-ax.plot(z, inter.Lagrange(x, y, z), label="Funkcja interpolacyjna")
+ax.plot(z, [inter.newton_interpolation(x, y, zz) for zz in z], label="Funkcja interpolacyjna")
 ax.scatter(x, y, label="Węzły interpolacji")
 ax.legend()
 plt.show()
--- numlib/interpolation.py
+++ numlib/interpolation.py
@@ -10,3 +10,30 @@ def Lagrange(x,y,z):
                 p *= (z-x[j])/(x[i]-x[j])
         s += y[i]*p
     return s
+
+def divided_diff(x, y):
+    n = len(x)
+    if n != len(y):
+        raise ValueError("Lista x i y muszą mieć taką samą długość")
+    # Inicjalizacja tablicy różnic dzielonych
+    F = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        F[i][0] = y[i]
+
+    # Obliczanie różnic dzielonych
+    for j in range(1, n):
+        for i in range(n-j):
+            F[i][j] = (F[i+1][j-1] - F[i][j-1]) / (x[i+j] - x[i])
+
+    return F[0]
+def newton_interpolation(x, y, z):
+    n = len(x)
+    if n != len(y):
+        raise ValueError("Lista x i y muszą mieć taką samą długość")
+
+    F = divided_diff(x, y)
+    p = F[n-1]
+    for k in range(1, n):
+        p = F[n-1-k] + (z - x[n-1-k])*p
+
+    return p
