d1e7efb968e4d0d228e1bf308f47fe8926ca25cd
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-03-06 17:40:39 +0100

    2

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-# x=np.array([-5,-4,-3,-2,-1,0,1,2,3,4,5])
-# y=np.array([25,16,9,4,25,0,1,4,9,2,25])
-# z=np.linspace(-5,5,100)
-# fig, ax = plt.subplots()
-# ax.plot(z, inter.Lagrange(x, y, z), label="Funkcja interpolacyjna")
-# ax.scatter(x, y, label="Węzły interpolacji")
-# ax.legend()
-# plt.show()
+x=np.array([-5,-4,-3,-2,-1,0,1,2,3,4,5])
+y=np.array([25,16,9,4,1,0,1,4,9,16,25])
+z=np.linspace(-5,5,100)
+fig, ax = plt.subplots()
+ax.plot(z, inter.Lagrange(x, y, z), label="Funkcja interpolacyjna")
+ax.scatter(x, y, label="Węzły interpolacji")
+plt.title("interpolacja Lagranga")
+ax.legend()
+plt.show()
 
 x = [0, 1, 2, 3, 4, 5]
 y = [0, 1, 4, 9, 16, 25]
@@ -22,5 +23,6 @@ z = np.linspace(0, 5, 100)
 fig, ax = plt.subplots()
 ax.plot(z, [inter.newton_interpolation(x, y, zz) for zz in z], label="Funkcja interpolacyjna")
 ax.scatter(x, y, label="Węzły interpolacji")
+plt.title("interpolacjny wielomian Newtona")
 ax.legend()
 plt.show()
